445f9c47dd4cace7218fa913015ef696a2f74c54
amaliarc <97264713+amaliarc@users.noreply.github.com>
2022-01-07 21:26:35 +0700

    Update regression_dev.py

--- regression_dev.py
+++ regression_dev.py
@@ -1,26 +1,36 @@
-from flask import Flask, render_template, request, jsonify
-import joblib
 import numpy as np
+from flask import Flask, request, jsonify, render_template
 import pickle
-import itsdangerous
-import Werkzeug
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
 @app.route('/')
-def student():
-   return render_template("home.html")
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(-1,1)
-    loaded_model = joblib.load('model.sav')
-    result = loaded_model.predict(to_predict)
-    return result[0]
-@app.route('/',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-    to_predict_list = request.form.to_dict()
-    to_predict_list=list(to_predict_list.values())
-    to_predict_list = list(map(float, to_predict_list))
-    result = float(ValuePredictor(to_predict_list))
-    return render_template("home.html",result = result)
-if __name__ == '__main__':
-   app.run(debug = True)
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Predict Income should be $ {}'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
